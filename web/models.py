6782f211d938ed556c705554b4490f3deeef43f8
Hamidnet220 <kiani.hamidreza@gmail.com>
2019-02-19 16:46:05 +0330

    add Expense model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Expense(models.Model):
+    text   = models.CharField(max_length = 255)
+    date   = models.DateTimeField()
+    amount = models.DecimalField(max_digits= 10 ,decimal_places=2) 
+    user   = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}-{}".format(self.text,self.date)
